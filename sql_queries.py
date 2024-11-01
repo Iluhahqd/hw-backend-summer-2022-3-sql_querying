1e40a172ff58a7570c59a77aa5b1381c9af0f113
Iluha <groaj@mail.ru>
2024-11-01 08:11:59 +0300

    fixed

--- sql_queries.py
+++ sql_queries.py
@@ -2,7 +2,10 @@
 # Duration - разница между scheduled_arrival и scheduled_departure.
 # В ответе должно быть 2 колонки [flight_no, duration]
 TASK_1_QUERY = """
-SELECT flight_no, (scheduled_arrival - scheduled_departure) AS duration FROM flights ORDER BY duration ASC LIMIT 5;
+	SELECT flight_no, (scheduled_arrival - scheduled_departure) as Duration
+ 	FROM flights
+	ORDER BY duration 
+	LIMIT 5;
 """
 #  flight_no | duration
 # -----------+----------
@@ -17,7 +20,12 @@ SELECT flight_no, (scheduled_arrival - scheduled_departure) AS duration FROM fli
 # количество упоминаний которых меньше 50
 # В ответе должно быть 2 колонки [flight_no, count]
 TASK_2_QUERY = """
-SELECT flight_no, COUNT(*) AS count FROM flights zGROUP BY flight_no HAVING COUNT(*) < 50 ORDER BY count DESC LIMIT 3;
+	SELECT flight_no, count(flight_no) as count 
+	FROM flights
+	GROUP BY flight_no 
+	HAVING count(flight_no ) < 50
+	ORDER BY count(flight_no) DESC LIMIT 3;
+
 """
 #  flight_no | count
 # -----------+-------
@@ -29,15 +37,13 @@ SELECT flight_no, COUNT(*) AS count FROM flights zGROUP BY flight_no HAVING COUN
 # Нужно вывести 1 значение в колонке count
 TASK_3_QUERY = """
 SELECT 
-    COUNT(*) AS count
-FROM 
-    flights
-WHERE 
-    timezone IS NOT NULL
-GROUP BY 
-    timezone
-HAVING 
-    COUNT(*) > 1;
+    (SELECT COUNT(*) FROM flights AS f_inner
+     JOIN airports_data AS dep_inner ON f_inner.departure_airport = dep_inner.airport_code) 
+    -
+    (SELECT COUNT(*) FROM flights AS f_outer
+     JOIN airports_data AS dep_outer ON f_outer.departure_airport = dep_outer.airport_code
+     JOIN airports_data AS arr_outer ON f_outer.arrival_airport = arr_outer.airport_code
+     WHERE dep_outer.timezone <> arr_outer.timezone) AS count;
 """
 #  count
 # --------
